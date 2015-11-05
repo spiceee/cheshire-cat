713be51795532a7145ad83aa0b817a2e4151b295
Spiceee <spiceee@gmail.com>
2015-11-05 11:27:16 -0200

    enfocus/effects

--- src-cljs/core.cljs
+++ src-cljs/core.cljs
@@ -4,9 +4,16 @@
             [cljs-http.client :as http]
             [cljs.core.async :refer [<!]]
             [enfocus.core :as ef]
-            [enfocus.events :as ev]))
+            [enfocus.events :as ev]
+            [enfocus.effects :as ee]))
 
 (enable-console-print!)
+
+(defn say-goodbye []
+  (ef/at
+   "#cat-name" (ee/fade-out 500)
+   "#button1" (ee/fade-out 500)
+   "#status" (ee/fade-out 5000)))
 
 (defn ^:export init []
   (repl/connect "http://localhost:9000/repl")
@@ -16,5 +23,5 @@
             "#status" (ef/do->
                        (ef/content (:status body))
                        (ef/set-style :font-size "500%")))
-     (ef/at "#button1" (ev/listen :click #(js/alert "Goodbye!"))))))
+     (ef/at "#button1" (ev/listen :click #(say-goodbye))))))
 
